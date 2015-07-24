c3bc060a817ed9557deb90b520960b5a6e7f7605
Andrey Yashnev <andjash@gmail.com>
2015-07-24 17:25:51 +0400

    Initial commit

---
+++ InstaProwler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaProwler
+//
+//  Created by Andrey Yashnev on 24/07/15.
+//  Copyright (c) 2015 Andrey Yashnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaProwler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaProwler
+//
+//  Created by Andrey Yashnev on 24/07/15.
+//  Copyright (c) 2015 Andrey Yashnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
